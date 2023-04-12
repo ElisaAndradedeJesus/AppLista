d979dac56643cbfa044584b7b4e06da430bd499b
20201TIIMI0225 <elisaaj.es@gmail.com>
2023-04-12 10:10:30 -0300

    ultimas atualizações

--- app/src/main/java/jesus/de/andrade/elisa/lista/activity/MainActivity.java
+++ app/src/main/java/jesus/de/andrade/elisa/lista/activity/MainActivity.java
@@ -2,15 +2,34 @@ package jesus.de.andrade.elisa.lista.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import jesus.de.andrade.elisa.lista.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int New_ITEM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Código que adicionei ↓
+
+        FloatingActionButton fabAddNewItem = findViewById(R.id.fabAddNewItem);
+        fabAddNewItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, NewItemActivity.class);
+                startActivityForResult(i, New_ITEM_REQUEST);
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/jesus/de/andrade/elisa/lista/activity/NewItemActivity.java
+++ app/src/main/java/jesus/de/andrade/elisa/lista/activity/NewItemActivity.java
@@ -1,16 +1,52 @@
 package jesus.de.andrade.elisa.lista.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import jesus.de.andrade.elisa.lista.R;
 
 public class NewItemActivity extends AppCompatActivity {
+    // some attributes (～￣▽￣)～
+    static  int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+
+        //Adding code ↓ ~(￣▽￣~)
+        ImageButton imgCI = findViewById(R.id.imbCI);
+        imgCI.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent,PHOTO_PICKER_REQUEST);
+            }// end
+
+        });// end imgCI listener ♪(´▽｀)
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode, data);
+        if(requestCode == PHOTO_PICKER_REQUEST){
+            if(resultCode == Activity.RESULT_OK){
+                photoSelected = data.getData();
+                ImageView imvFotoPreview = findViewById(R.id.imvPhotoPreview);
+                imvFotoPreview.setImageURI(photoSelected);
+            }
+        }
     }
 }
